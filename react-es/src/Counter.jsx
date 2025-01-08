2ae1d36f65a54061146d30801da91655ff50a92f
Rodobrodo2 <rodondolo2@gmail.com>
2025-01-08 13:09:33 +0100

    Sincronizzare il titolo con il contatore

--- react-es/src/Counter.jsx
+++ react-es/src/Counter.jsx
@@ -1,17 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Counter = () => {
-
     const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        const h1Element = document.getElementById("count-heading");
+        if (h1Element) {
+            h1Element.innerText = `Count: ${count}`;
+        }
+        
+    }, [count]); 
 
     return (
         <div>
-            <p>Count: {count}</p>
+            <h1 id="count-heading">Count: {count}</h1>
             <button onClick={() => setCount(count + 1)}>Increment</button>
             <button onClick={() => setCount(count - 1)}>Decrement</button>
             <button onClick={() => setCount(0)}>Reset</button>
         </div>
-    )
+    );
 };
 
 export default Counter;
