989c0e2012f80643483623618dc8a9fa46f585d8
Rodobrodo2 <rodondolo2@gmail.com>
2025-01-17 15:45:47 +0100

    useMemo

--- react-es/src/TodoList.jsx
+++ react-es/src/TodoList.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import useFetch from "./hooks/useFetch";
 import useFilteredTodos from "./hooks/useFilteredTodos";
-import { useState, useCallback } from "react";
+import { useState, useCallback, useMemo } from "react";
 
 const API_URL = "https://jsonplaceholder.typicode.com/todos";
 
 const TodoList = () => {
   const { data, isLoading, error } = useFetch(API_URL);
   const  [searchTerm, setSearchTerm] = useState("");
-  const filteredTodos = useFilteredTodos(data || [], searchTerm);
-
+  
   // Memorizza la funzione di gestione dell'input con useCallback
   const handleSearchChange = useCallback((e) => {
     setSearchTerm(e.target.value);
   }, []); // Nessuna dipendenza: la funzione non cambi
+
+  const filteredTodos = useMemo(() => {
+    if (!data) return []; // Gestisce il caso in cui i dati non sono ancora caricati
+    return data.filter((todo) =>
+      todo.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }, [data, searchTerm]); // Ricalcola solo quando `data` o `searchTerm` cambiano
 
   if (isLoading) {
     return <p>Caricamento in corso...</p>;
